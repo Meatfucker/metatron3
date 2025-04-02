60edb0a0782c5b1ccf3eea52baa4d038c3bfc6fc
Meatfucker <tramshed@gmail.com>
2025-04-01 23:11:52 -0400

    Added windows install and startup scripts. Added reroll, dm, and delete buttons to sdxl and flux gens.

--- modules/flux.py
+++ modules/flux.py
@@ -1,5 +1,6 @@
 import base64
 import io
+import re
 import discord
 from loguru import logger
 from modules.settings_loader import SettingsLoader
@@ -17,6 +18,7 @@ class FluxGen:
                  batch_size=1):
         self.settings = SettingsLoader("configs")
         self.discord_client = discord_client
+        self.avernus_client = discord_client.avernus_client
         self.prompt = prompt
         self.channel = channel
         self.user = user
@@ -27,22 +29,30 @@ class FluxGen:
 
     async def run(self):
         if self.lora_name:
-            base64_images = await self.discord_client.avernus_client.flux_image(self.prompt,
-                                                                                batch_size=self.batch_size,
-                                                                                width=self.width,
-                                                                                height=self.height,
-                                                                                lora_name=self.lora_name)
+            base64_images = await self.avernus_client.flux_image(self.prompt,
+                                                                 batch_size=self.batch_size,
+                                                                 width=self.width,
+                                                                 height=self.height,
+                                                                 lora_name=self.lora_name)
         else:
-            base64_images = await self.discord_client.avernus_client.flux_image(self.prompt,
-                                                                                batch_size=self.batch_size,
-                                                                                width=self.width,
-                                                                                height=self.height,
-                                                                                )
+            base64_images = await self.avernus_client.flux_image(self.prompt,
+                                                                 batch_size=self.batch_size,
+                                                                 width=self.width,
+                                                                 height=self.height)
         images = await self.base64_to_pil_images(base64_images)
         files = await self.images_to_discord_files(images)
         await self.channel.send(
-            content=f"Flux Gen for `{self.user}`: Prompt: `{self.prompt}`",
-            files=files)
+            content=f"Flux Gen for {self.user.mention}: Prompt: `{self.prompt}`",
+            files=files,
+            view=FluxButtons(self.discord_client,
+                             self.prompt,
+                             self.channel,
+                             self.user,
+                             self.width,
+                             self.height,
+                             self.lora_name,
+                             self.batch_size))
+
         sdxl_logger = logger.bind(user=f'{self.user}', prompt=self.prompt)
         sdxl_logger.info("Flux Success")
 
@@ -70,15 +80,15 @@ class FluxGen:
 
 class FluxGenEnhanced(FluxGen):
     async def run(self):
-        enhanced_prompt = await self.discord_client.avernus_client.llm_chat(f"Turn the following prompt into a three sentence visual description of it. Here is the prompt: {self.prompt}")
+        enhanced_prompt = await self.avernus_client.llm_chat(f"Turn the following prompt into a three sentence visual description of it. Here is the prompt: {self.prompt}")
         if self.lora_name:
-            base64_images = await self.discord_client.avernus_client.flux_image(enhanced_prompt,
+            base64_images = await self.avernus_client.flux_image(enhanced_prompt,
                                                                                 batch_size=self.batch_size,
                                                                                 width=self.width,
                                                                                 height=self.height,
                                                                                 lora_name=self.lora_name)
         else:
-            base64_images = await self.discord_client.avernus_client.flux_image(enhanced_prompt,
+            base64_images = await self.avernus_client.flux_image(enhanced_prompt,
                                                                                 batch_size=self.batch_size,
                                                                                 width=self.width,
                                                                                 height=self.height,
@@ -86,7 +96,106 @@ class FluxGenEnhanced(FluxGen):
         images = await self.base64_to_pil_images(base64_images)
         files = await self.images_to_discord_files(images)
         await self.channel.send(
-            content=f"Flux Gen for:`{self.user}` Prompt:`{self.prompt}` Enhanced Prompt:`{enhanced_prompt}`",
-            files=files)
+            content=f"Flux Gen for: {self.user.mention} Prompt:`{self.prompt}` Enhanced Prompt:`{enhanced_prompt}`",
+            files=files,
+            view=FluxEnhancedButtons(self.discord_client,
+                                     self.prompt,
+                                     self.channel,
+                                     self.user,
+                                     self.width,
+                                     self.height,
+                                     self.lora_name,
+                                     self.batch_size))
+
         sdxl_logger = logger.bind(user=f'{self.user}', prompt=self.prompt)
         sdxl_logger.info("Flux Success")
+
+class FluxButtons(discord.ui.View):
+    """Class for the ui buttons on /flux_gen"""
+    def __init__(self,
+                 discord_client,
+                 prompt,
+                 channel,
+                 user,
+                 width,
+                 height,
+                 lora_name=None,
+                 batch_size=4):
+        super().__init__()
+        self.timeout = None  # Disables the timeout on the buttons
+        self.discord_client = discord_client
+        self.prompt = prompt
+        self.channel = channel
+        self.user = user
+        self.width = width
+        self.height = height
+        self.lora_name = lora_name
+        self.batch_size = batch_size
+
+    @discord.ui.button(label='Reroll', emoji="🎲", style=discord.ButtonStyle.grey)
+    async def reroll(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Rerolls last Flux gen"""
+        if self.user.id == interaction.user.id:
+            if await self.discord_client.is_room_in_queue(self.user.id):
+                flux_request = FluxGen(self.discord_client,
+                                       self.prompt,
+                                       self.channel,
+                                       self.user,
+                                       width=self.width,
+                                       height=self.height,
+                                       batch_size=self.batch_size,
+                                       lora_name=self.lora_name)
+                await interaction.response.send_message("Rerolling...", ephemeral=True, delete_after=5)
+                flux_queuelogger = logger.bind(user=self.user.name, prompt=self.prompt)
+                flux_queuelogger.info("Flux Queued")
+                self.discord_client.request_queue_concurrency_list[self.user.id] += 1
+                await self.discord_client.request_queue.put(flux_request)
+            else:
+                await interaction.response.send_message("Queue limit reached, please wait until your current gen or gens finish")
+
+    @discord.ui.button(label='Mail', emoji="✉", style=discord.ButtonStyle.grey)
+    async def dmimage(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """DMs Flux Image"""
+        await interaction.response.send_message("DM'ing image...", ephemeral=True, delete_after=5)
+        sanitized_prompt = re.sub(r'[^\w\s\-.]', '', self.prompt)[:100]
+        files = []
+        for file in interaction.message.attachments:
+            image_bytes = await file.read()
+            attachment = discord.File(io.BytesIO(image_bytes), filename=f'{sanitized_prompt}.png')
+            files.append(attachment)
+        dm_channel = await interaction.user.create_dm()
+        await dm_channel.send(content=self.prompt, files=files)
+        image_dm_logger = logger.bind(user=interaction.user.name, userid=interaction.user.id)
+        image_dm_logger.success("Flux DM successful")
+
+    @discord.ui.button(label='Delete', emoji="❌", style=discord.ButtonStyle.grey)
+    async def delete_message(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Deletes message"""
+        if self.user.id == interaction.user.id:
+            await interaction.message.delete()
+        await interaction.response.send_message("Image deleted.", ephemeral=True, delete_after=5)
+        speak_delete_logger = logger.bind(user=interaction.user.name, userid=interaction.user.id)
+        speak_delete_logger.info("Flux Delete")
+
+class FluxEnhancedButtons(FluxButtons):
+    """Class for the prompt enhanced ui buttons on /flux_gen"""
+    @discord.ui.button(label='Reroll', emoji="🎲", style=discord.ButtonStyle.grey)
+    async def reroll(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Rerolls last flux enhanced gen"""
+        if self.user.id == interaction.user.id:
+            if await self.discord_client.is_room_in_queue(self.user.id):
+                flux_request = FluxGenEnhanced(self.discord_client,
+                                               self.prompt,
+                                               self.channel,
+                                               self.user,
+                                               width=self.width,
+                                               height=self.height,
+                                               batch_size=self.batch_size,
+                                               lora_name=self.lora_name)
+                await interaction.response.send_message("Rerolling...", ephemeral=True, delete_after=5)
+                flux_queuelogger = logger.bind(user=self.user.name, prompt=self.prompt)
+                flux_queuelogger.info("Flux Queued")
+                self.discord_client.request_queue_concurrency_list[self.user.id] += 1
+                await self.discord_client.request_queue.put(flux_request)
+            else:
+                await interaction.response.send_message("Queue limit reached, please wait until your current gen or gens finish")
--- modules/sdxl.py
+++ modules/sdxl.py
@@ -1,8 +1,11 @@
 import base64
 import io
+import re
 import discord
 from loguru import logger
 from modules.settings_loader import SettingsLoader
+
+
 
 class SDXLGen:
     """This is the queue object for sdxl generations"""
@@ -18,6 +21,7 @@ class SDXLGen:
                  batch_size=4):
         self.settings = SettingsLoader("configs")
         self.discord_client = discord_client
+        self.avernus_client = discord_client.avernus_client
         self.prompt = prompt
         self.channel = channel
         self.user = user
@@ -29,27 +33,34 @@ class SDXLGen:
 
     async def run(self):
         if self.lora_name:
-            base64_images = await self.discord_client.avernus_client.sdxl_image(self.prompt,
-                                                                                batch_size=self.batch_size,
-                                                                                negative_prompt=self.negative_prompt,
-                                                                                width=self.width,
-                                                                                height=self.height,
-                                                                                lora_name=self.lora_name)
+            base64_images = await self.avernus_client.sdxl_image(self.prompt,
+                                                                 batch_size=self.batch_size,
+                                                                 negative_prompt=self.negative_prompt,
+                                                                 width=self.width,
+                                                                 height=self.height,
+                                                                 lora_name=self.lora_name)
         else:
-            base64_images = await self.discord_client.avernus_client.sdxl_image(self.prompt,
-                                                                                batch_size=self.batch_size,
-                                                                                negative_prompt=self.negative_prompt,
-                                                                                width=self.width,
-                                                                                height=self.height,
-                                                                                )
+            base64_images = await self.avernus_client.sdxl_image(self.prompt,
+                                                                 batch_size=self.batch_size,
+                                                                 negative_prompt=self.negative_prompt,
+                                                                 width=self.width,
+                                                                 height=self.height)
         images = await self.base64_to_pil_images(base64_images)
         files = await self.images_to_discord_files(images)
         await self.channel.send(
-            content=f"SDXL Gen for `{self.user}`: Prompt: `{self.prompt}`",
-            files=files)
+            content=f"SDXL Gen for {self.user.mention}: Prompt: `{self.prompt}`",
+            files=files,
+            view=SDXLButtons(self.discord_client,
+                             self.prompt,
+                             self.channel,
+                             self.user,
+                             self.width,
+                             self.height,
+                             self.negative_prompt,
+                             self.lora_name,
+                             self.batch_size))
         sdxl_logger = logger.bind(user=f'{self.user}', prompt=self.prompt)
         sdxl_logger.info("SDXL Success")
-
 
     async def images_to_discord_files(self, images):
         """Takes a list of images or image objects and returns a list of discord file objects"""
@@ -71,18 +82,19 @@ class SDXLGen:
 
         return image_files
 
+
 class SDXLGenEnhanced(SDXLGen):
     async def run(self):
-        enhanced_prompt = await self.discord_client.avernus_client.llm_chat(f"Turn the following prompt into a three sentence visual description of it. Here is the prompt: {self.prompt}")
+        enhanced_prompt = await self.avernus_client.llm_chat(f"Turn the following prompt into a three sentence visual description of it. Here is the prompt: {self.prompt}")
         if self.lora_name:
-            base64_images = await self.discord_client.avernus_client.sdxl_image(enhanced_prompt,
+            base64_images = await self.avernus_client.sdxl_image(enhanced_prompt,
                                                                                 batch_size=self.batch_size,
                                                                                 negative_prompt=self.negative_prompt,
                                                                                 width=self.width,
                                                                                 height=self.height,
                                                                                 lora_name=self.lora_name)
         else:
-            base64_images = await self.discord_client.avernus_client.sdxl_image(enhanced_prompt,
+            base64_images = await self.avernus_client.sdxl_image(enhanced_prompt,
                                                                                 batch_size=self.batch_size,
                                                                                 negative_prompt=self.negative_prompt,
                                                                                 width=self.width,
@@ -92,6 +104,110 @@ class SDXLGenEnhanced(SDXLGen):
         files = await self.images_to_discord_files(images)
         await self.channel.send(
             content=f"SDXL Gen for:`{self.user}` Prompt:`{self.prompt}` Enhanced Prompt:`{enhanced_prompt}`",
-            files=files)
+            files=files,
+            view=SDXLEnhancedButtons(self.discord_client,
+                                     self.prompt,
+                                     self.channel,
+                                     self.user,
+                                     self.width,
+                                     self.height,
+                                     self.negative_prompt,
+                                     self.lora_name,
+                                     self.batch_size))
         sdxl_logger = logger.bind(user=f'{self.user}', prompt=self.prompt)
         sdxl_logger.info("SDXL Success")
+
+
+class SDXLButtons(discord.ui.View):
+    """Class for the ui buttons on /sdxl_gen"""
+    def __init__(self,
+                 discord_client,
+                 prompt,
+                 channel,
+                 user,
+                 width,
+                 height,
+                 negative_prompt=None,
+                 lora_name=None,
+                 batch_size=4):
+        super().__init__()
+        self.timeout = None  # Disables the timeout on the buttons
+        self.discord_client = discord_client
+        self.prompt = prompt
+        self.channel = channel
+        self.user = user
+        self.negative_prompt = negative_prompt
+        self.width = width
+        self.height = height
+        self.lora_name = lora_name
+        self.batch_size = batch_size
+
+    @discord.ui.button(label='Reroll', emoji="🎲", style=discord.ButtonStyle.grey)
+    async def reroll(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Rerolls last SDXL gen"""
+        if self.user.id == interaction.user.id:
+            if await self.discord_client.is_room_in_queue(self.user.id):
+                sdxl_request = SDXLGen(self.discord_client,
+                                       self.prompt,
+                                       self.channel,
+                                       self.user,
+                                       negative_prompt=self.negative_prompt,
+                                       width=self.width,
+                                       height=self.height,
+                                       batch_size=self.batch_size,
+                                       lora_name=self.lora_name)
+                await interaction.response.send_message("Rerolling...", ephemeral=True, delete_after=5)
+                sdxl_queuelogger = logger.bind(user=self.user.name, prompt=self.prompt)
+                sdxl_queuelogger.info("SDXL Queued")
+                self.discord_client.request_queue_concurrency_list[self.user.id] += 1
+                await self.discord_client.request_queue.put(sdxl_request)
+            else:
+                await interaction.response.send_message("Queue limit reached, please wait until your current gen or gens finish")
+
+    @discord.ui.button(label='Mail', emoji="✉", style=discord.ButtonStyle.grey)
+    async def dmimage(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """DMs SDXL Image"""
+        await interaction.response.send_message("DM'ing image...", ephemeral=True, delete_after=5)
+        sanitized_prompt = re.sub(r'[^\w\s\-.]', '', self.prompt)[:100]
+        files = []
+        for file in interaction.message.attachments:
+            image_bytes = await file.read()
+            attachment = discord.File(io.BytesIO(image_bytes), filename=f'{sanitized_prompt}.png')
+            files.append(attachment)
+        dm_channel = await interaction.user.create_dm()
+        await dm_channel.send(content=self.prompt, files=files)
+        image_dm_logger = logger.bind(user=interaction.user.name, userid=interaction.user.id)
+        image_dm_logger.success("SDXL DM successful")
+
+    @discord.ui.button(label='Delete', emoji="❌", style=discord.ButtonStyle.grey)
+    async def delete_message(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Deletes message"""
+        if self.user.id == interaction.user.id:
+            await interaction.message.delete()
+        await interaction.response.send_message("Image deleted.", ephemeral=True, delete_after=5)
+        speak_delete_logger = logger.bind(user=interaction.user.name, userid=interaction.user.id)
+        speak_delete_logger.info("IMAGEGEN Delete")
+
+class SDXLEnhancedButtons(SDXLButtons):
+    """Class for the prompt enhanced ui buttons on /sdxl_gen"""
+    @discord.ui.button(label='Reroll', emoji="🎲", style=discord.ButtonStyle.grey)
+    async def reroll(self, interaction: discord.Interaction, button: discord.ui.Button):
+        """Rerolls last SDXL enhanced gen"""
+        if self.user.id == interaction.user.id:
+            if await self.discord_client.is_room_in_queue(self.user.id):
+                sdxl_request = SDXLGenEnhanced(self.discord_client,
+                                               self.prompt,
+                                               self.channel,
+                                               self.user,
+                                               negative_prompt=self.negative_prompt,
+                                               width=self.width,
+                                               height=self.height,
+                                               batch_size=self.batch_size,
+                                               lora_name=self.lora_name)
+                await interaction.response.send_message("Rerolling...", ephemeral=True, delete_after=5)
+                sdxl_queuelogger = logger.bind(user=self.user.name, prompt=self.prompt)
+                sdxl_queuelogger.info("SDXL Queued")
+                self.discord_client.request_queue_concurrency_list[self.user.id] += 1
+                await self.discord_client.request_queue.put(sdxl_request)
+            else:
+                await interaction.response.send_message("Queue limit reached, please wait until your current gen or gens finish")
